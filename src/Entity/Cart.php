4fddfc7cc7b30972de7ace692a2cab005f4106ed
Benway Teh <teh.benway@gmail.com>
2024-05-31 17:56:13 +0200

    ajout entity Cart et son controller

---
+++ src/Entity/Cart.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CartRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CartRepository::class)]
+class Cart
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::ARRAY)]
+    private array $articles = [];
+
+    #[ORM\Column]
+    private ?int $userId = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getArticles(): array
+    {
+        return $this->articles;
+    }
+
+    public function setArticles(array $articles): static
+    {
+        $this->articles = $articles;
+
+        return $this;
+    }
+
+    public function getUserId(): ?int
+    {
+        return $this->userId;
+    }
+
+    public function setUserId(int $userId): static
+    {
+        $this->userId = $userId;
+
+        return $this;
+    }
+}
