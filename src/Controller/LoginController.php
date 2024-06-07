64e84827dd4a40d4a5e92b94f22f3964ad67bba5
Dradoke <nathan.gaudouin76190@gmail.com>
2024-06-07 14:03:02 +0200

    Ajout header

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 class LoginController extends AbstractController
 {
@@ -24,9 +25,13 @@ class LoginController extends AbstractController
         ]);
     }
 
+
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
+
+
+
 }
